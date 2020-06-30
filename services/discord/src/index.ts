23a0d26be381fbe578e662da9e114171ad434c27
Dan Lindsey <dan@mdlindsey.com>
2020-06-29 22:23:53 -0400

    Start Discord bot on Express listener

--- services/discord/src/index.ts
+++ services/discord/src/index.ts
@@ -3,12 +3,9 @@ import * as express from 'express'
 import DiscordBot from '@stagg/discord'
 import cfg from './config'
 
-// JWT.Config(cfg.jwt)
-// Mail.Config(cfg.gmail)
-// Mail.SendConfirmation('dan@mdlindsey.com')
-new DiscordBot(cfg.discord.token, cfg.jwt, cfg.gmail, cfg.mongo)
 const app = express()
 app.use(cors({ credentials: false })).listen(cfg.port, async () => {
+    new DiscordBot(cfg.discord.token, cfg.jwt, cfg.gmail, cfg.mongo)
     app.get('/', (req,res) => res.redirect('https://discord.me/ggez'))
     app.get('/health', (req,res) => res.status(200).send('ok'))
     console.log(
