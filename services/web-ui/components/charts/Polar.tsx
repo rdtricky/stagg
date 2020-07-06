44e79666fa32a56926f2ef145cb48d483497f687
Dan Lindsey <dan@mdlindsey.com>
2020-07-06 15:16:32 -0400

    Mode color standardization

--- services/web-ui/components/charts/Polar.tsx
+++ services/web-ui/components/charts/Polar.tsx
@@ -8,12 +8,15 @@ export default ({ groups }) => {
       label: '',
       data: Object.values(groups),
       backgroundColor: [
-        '#4BC0C0',
-        '#FFCE56',
-        '#E7E9ED',
-        '#36A2EB',
-        '#FF6384',
-      ]
+				'#003f5c',
+				'#2f4b7c',
+				'#665191',
+				'#a05195',
+				'#d45087',
+				'#f95d6a',
+				'#ff7c43',
+				'#ffa600',
+			]
     }]
   }
   return <Polar options={options} data={data} />
--- services/web-ui/pages/wz/[id]/charts/GamesByMode.tsx
+++ services/web-ui/pages/wz/[id]/charts/GamesByMode.tsx
@@ -2,16 +2,19 @@ import { Map } from '@stagg/api'
 import Pie from '../../../../components/charts/Pie'
 
 export default ({ performances }) => {
-    const groups = {}
+    const groups = {
+        Solos: 0,
+        Duos:  0,
+        Trios: 0,
+        Quads: 0,
+    }
     for(const p of performances) {
         const mode = Map.CallOfDuty.Modes[p.modeId]
         if (!mode) {
             console.log(`No mode for "${p.modeId}" in`, Map.CallOfDuty.Modes)
             continue
         }
-        const teamSizeLabels = ['Solos', 'Duos', 'Trios', 'Quads']
-        if (!groups[teamSizeLabels[mode.teamSize-1]]) groups[teamSizeLabels[mode.teamSize-1]] = 0
-        groups[teamSizeLabels[mode.teamSize-1]]++
+        groups[Object.keys(groups)[mode.teamSize-1]]++
     }
     return <Pie groups={groups} />
 }
