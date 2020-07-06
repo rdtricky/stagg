42532227a9cdeebad16e2301e0ce01c90ff5c1d7
Dan Lindsey <dan@mdlindsey.com>
2020-07-06 10:57:34 -0400

    Fix build

--- services/web-ui/next.config.js
+++ services/web-ui/next.config.js
@@ -4,4 +4,13 @@ module.exports = {
     env: process.env.NODE_ENV,
     port: process.env.PORT,
   },
+  // add webpack config so the request dep in @stagg/api doesn't break client-side for lack of these modules
+  webpack: (config) => ({
+    ...config,
+    node: {
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty',
+    }
+  })
 }
